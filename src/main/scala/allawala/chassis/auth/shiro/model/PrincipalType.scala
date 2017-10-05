a390154c7dc4be5fd6b086a9314d65018fb27c32
Omar Allawala <allawala@Omars-MBP.lan>
2017-10-05 11:15:42 +1100

    Add ImpersonatedUser principal type
    https://github.com/allawala/service-chassis/issues/36
    
    Resolves: #36

--- src/main/scala/allawala/chassis/auth/shiro/model/PrincipalType.scala
+++ src/main/scala/allawala/chassis/auth/shiro/model/PrincipalType.scala
@@ -9,6 +9,12 @@ case object PrincipalType extends Enum[PrincipalType] with CirceEnum[PrincipalTy
 
   case object Service extends PrincipalType
   case object User extends PrincipalType
+  /*
+    Eg. "service" principal type enquires whether a certain user is permitted to perform some action. This allows us to have
+    flexibility in defining our authentication logic differently for the two user types if required and allowing us to get a
+    Shiro subject representing the impersonated user.
+  */
+  case object ImpersonatedUser extends PrincipalType
 
   val values = findValues
 }
